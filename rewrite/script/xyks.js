63ce2c969aa7e5eaa3c8c566ecc7207a308d1b1a
just22tu <just22tu@gmail.com>
2024-10-11 16:52:44 +0800

    update xyks.js

--- rewrite/script/xyks.js
+++ rewrite/script/xyks.js
@@ -1,23 +1,34 @@
 let body = $response.body;
 let obj = JSON.parse(body);
 
-if (obj.examVO && obj.examVO.questions && obj.examVO.questions.length > 0) {
-    // obj.examVO.updatedTime = 0; 
-    // obj.examVO.correctCnt = obj.examVO.questionCnt;
-    // obj.examVO.costTime = 0;
+// 获取当前请求的URL
+const url = $request.url;
 
-    const firstQuestion = obj.examVO.questions[0];
-    obj.examVO.questions = firstQuestion ? [firstQuestion] : [];
+// 匹配不同的路径
+const isPK = /\/leo-game-pk\/.+\/pk\/match/.test(url);
+const isExams = /\/leo-math\/.+\/exams/.test(url);
 
-    if (firstQuestion && firstQuestion.answer) {
-        firstQuestion.answers = [""];
-
-        // firstQuestion.status = 1;
-
-        // firstQuestion.userAnswer = "";
-    }
-
-    body = JSON.stringify(obj);
+function processQuestions(questions) {
+    const firstQuestion = questions[0];
+    return firstQuestion ? [firstQuestion] : [];
 }
 
+// 处理PK逻辑
+if (isPK && obj.examVO && obj.examVO.questions && obj.examVO.questions.length > 0) {
+    obj.examVO.questions = processQuestions(obj.examVO.questions);
+    const firstQuestion = obj.examVO.questions[0];
+    if (firstQuestion && firstQuestion.answer) {
+        firstQuestion.answers = [""];
+    }
+
+// 处理练习场逻辑
+} else if (isExams && obj.questions && obj.questions.length > 0) {
+    obj.questions = processQuestions(obj.questions);
+    const firstQuestion = obj.questions[0];
+    if (firstQuestion && firstQuestion.answer) {
+        firstQuestion.answers = [""];
+    }
+}
+
+body = JSON.stringify(obj);
 $done({ body });
